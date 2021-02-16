e80809f3040a069bef4d6c2b43b1525099d41ab6
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-02-16 14:00:22 +1100

    Python-dill: use AR image

--- docker/python-dill/push.sh
+++ docker/python-dill/push.sh
@@ -5,6 +5,6 @@ do
     sed "s/@PYTHON_VERSION@/$version/g" Dockerfile > Dockerfile.out
     docker build --tag hailgenetics/python-dill:$version - <Dockerfile.out
     docker push hailgenetics/python-dill:$version
-    docker tag hailgenetics/python-dill:$version gcr.io/$PROJECT/python-dill:$version
-    docker push gcr.io/$PROJECT/python-dill:$version
+    docker tag hailgenetics/python-dill:$version $REGION-docker.pkg.dev/$PROJECT/hail/python-dill:$version
+    docker push $REGION-docker.pkg.dev/$PROJECT/hail/python-dill:$version
 done
