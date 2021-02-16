776a02390ea7e0619642c08ad607f5f63b03a847
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-02-16 13:03:37 +1100

    config.mk: use AR docker image

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 PROJECT := hail-295901
-DOCKER_ROOT_IMAGE := gcr.io/$(PROJECT)/ubuntu:18.04
+REGION := australia-southeast1
+ZONE := australia-southeast1-b
+DOCKER_ROOT_IMAGE := $(REGION)-docker.pkg.dev/$(PROJECT)/hail/ubuntu:18.04
 DOMAIN := hail.populationgenomics.org.au
 INTERNAL_IP := 10.152.0.2
 IP := 35.201.29.236
 KUBERNETES_SERVER_URL := https://34.87.199.41
-REGION := australia-southeast1
-ZONE := australia-southeast1-b
 ifeq ($(NAMESPACE),default)
 SCOPE = deploy
 DEPLOY = true
