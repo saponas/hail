83009a461cbf61131499b940817eec9e02aea66c
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-03-26 10:54:37 +1100

    Temporary disable user functions

--- hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
+++ hail/src/main/scala/is/hail/backend/service/ServiceBackend.scala
@@ -437,7 +437,7 @@ class ServiceBackend() extends Backend {
       userContext(username, timer) { ctx =>
         log.info(s"executing: ${token}")
         ctx.backendContext = new ServiceBackendContext(username, sessionID, billingProject, bucket)
-        registerUserFunctions()
+//        registerUserFunctions()
         execute(ctx, IRParser.parse_value_ir(ctx, code)) match {
           case Some((v, t)) =>
             JsonMethods.compact(
