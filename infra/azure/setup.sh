4542f270c477c1482f18f172cbed48dab3ea374a
Scott Saponas <scott@saponas.net>
2021-05-20 01:00:49 +0000

    Adding azure-cli and terraform to devcontainer.

--- infra/azure/setup.sh
+++ infra/azure/setup.sh
@@ -1,2 +1,8 @@
 # Install pip packages
-python3 -m pip install -r /workspaces/hail/docker/requirements.txt
+# python3 -m pip install -r /workspaces/hail/docker/requirements.txt
+
+# Report terraform version
+terraform -v
+
+# Report Azure CLI verison
+az --version
