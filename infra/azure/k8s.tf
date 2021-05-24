2544bf2289ef9e5b84d61133d2afcade7bbd711e
Miah Wander <miah@microsoft.com>
2021-05-24 20:42:07 +0000

    local variable name cleanup and user assigned id

--- infra/azure/k8s.tf
+++ infra/azure/k8s.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "k8s" {
+resource "azurerm_resource_group" "rg" {
     name     = var.resource_group_name
     location = var.location
 }
@@ -7,20 +7,20 @@ resource "random_id" "log_analytics_workspace_name_suffix" {
     byte_length = 8
 }
 
-resource "azurerm_log_analytics_workspace" "test" {
+resource "azurerm_log_analytics_workspace" "laworkspace" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
     name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
     location            = var.log_analytics_workspace_location
-    resource_group_name = azurerm_resource_group.k8s.name
+    resource_group_name = azurerm_resource_group.rg.name
     sku                 = var.log_analytics_workspace_sku
 }
 
 resource "azurerm_log_analytics_solution" "test" {
     solution_name         = "ContainerInsights"
-    location              = azurerm_log_analytics_workspace.test.location
-    resource_group_name   = azurerm_resource_group.k8s.name
-    workspace_resource_id = azurerm_log_analytics_workspace.test.id
-    workspace_name        = azurerm_log_analytics_workspace.test.name
+    location              = azurerm_log_analytics_workspace.laworkspace.location
+    resource_group_name   = azurerm_resource_group.rg.name
+    workspace_resource_id = azurerm_log_analytics_workspace.laworkspace.id
+    workspace_name        = azurerm_log_analytics_workspace.laworkspace.name
 
     plan {
         publisher = "Microsoft"
@@ -28,10 +28,16 @@ resource "azurerm_log_analytics_solution" "test" {
     }
 }
 
+resource "azurerm_user_assigned_identity" "uai" {
+    name                = "k8s_user_assigned_identity"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
 resource "azurerm_kubernetes_cluster" "k8s" {
     name                = var.cluster_name
-    location            = azurerm_resource_group.k8s.location
-    resource_group_name = azurerm_resource_group.k8s.name
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
     dns_prefix          = var.dns_prefix
 
     linux_profile {
@@ -50,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
     identity {
         type                      = "UserAssigned"
-        user_assigned_identity_id = var.user_assigned_identity_id
+        user_assigned_identity_id = azurerm_user_assigned_identity.uai.id
     }
 
     # service_principal {
@@ -61,7 +67,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     addon_profile {
         oms_agent {
         enabled                    = true
-        log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
+        log_analytics_workspace_id = azurerm_log_analytics_workspace.laworkspace.id
         }
     }
 
