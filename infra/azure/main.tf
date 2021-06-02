5bd9a8b5cfb3f3fa8ab2af701dfe9abc24408cc6
miah <miah@microsoft.com>
2021-06-02 15:41:23 +0000

    deployment WIP

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -20,12 +20,12 @@ provider "azurerm" {
   # apply the plan unless all required resource providers are registered by an account 
   # with appropriate privs.
   #
+  # skip_provider_registration=true
+  #
   # You can verify whether you have sufficient privs to register resource providers by 
   # running the following az cli command:
   #   `az provider register --subscription "YOURSUBSCRIPTION" -n Microsoft.Maps
   #
-  # TODO: consider using a service principal with necessary privs 
-  skip_provider_registration=true
   features {
   }
 }
