0b4bb9629aa22c4d2fb1ee971ed19663c582a6dd
Miah Wander <miah@microsoft.com>
2021-07-14 14:36:53 -0700

    node rg in terraform

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
-PROJECT := azhaildev2-rg
+PROJECT := hailtest1
 REGION := westus2
 ZONE := westus2
-DOCKER_PREFIX := azhaildev2acr.azurecr.io
+DOCKER_PREFIX := hailtest1acr.azurecr.io
 DOCKER_ROOT_IMAGE := $(DOCKER_PREFIX)/ubuntu:18.04
 DOMAIN := azhaildev2.azurewebsites.net
-INTERNAL_IP := 52.148.164.36
-IP := 40.91.87.11
+INTERNAL_IP := "TODO"
+IP := 20.69.76.201
 KUBERNETES_SERVER_URL := https://azhaildev2vdc-ab81f4ac.hcp.westus2.azmk8s.io
 ifeq ($(NAMESPACE),default)
 SCOPE = deploy
--- infra/azure/network.tf
+++ infra/azure/network.tf
@@ -12,17 +12,17 @@ resource "azurerm_subnet" "kubesubnet" {
   address_prefixes     = ["15.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "appgwsubnet" {
-  name                 = "appgwsubnet"
-  virtual_network_name = azurerm_virtual_network.default.name
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  address_prefixes     = ["15.1.0.0/16"]
-}
+#resource "azurerm_subnet" "appgwsubnet" {
+#  name                 = "appgwsubnet"
+#  virtual_network_name = azurerm_virtual_network.default.name
+#  resource_group_name  = data.azurerm_resource_group.rg.name
+#  address_prefixes     = ["15.1.0.0/16"]
+#}
 
 resource "azurerm_public_ip" "gateway" {
   name                = "gateway"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.node_rg.location
+  resource_group_name = data.azurerm_resource_group.node_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
